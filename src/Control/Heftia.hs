04360da6abb6b591c3e4c321ee972a09df353611
Yamada Ryo <ymdfield@outlook.jp>
2023-08-28 22:23:24 +0900

    [add] handling functions for Extensible Effects.

--- src/Control/Heftia.hs
+++ src/Control/Heftia.hs
@@ -5,7 +5,7 @@ module Control.Heftia where
 
 import Control.Hefty (HFunctor, LiftIns, hmap, unliftIns)
 import Control.Natural (type (~>))
-import Data.Hefty.Union (weakenSig, type (<:))
+import Data.Hefty.Union (Member, Union, decomp, inject, project, weakenL, weakenR, weakenSig, type (<:))
 
 class (forall sig. HFunctor sig => c (h sig)) => Heftia c h | h -> c where
     {-# MINIMAL liftSig, interpretH #-}
@@ -18,7 +18,7 @@ class (forall sig. HFunctor sig => c (h sig)) => Heftia c h | h -> c where
     -- | Translate /signature/s embedded in a Heftia monad.
     translateH ::
         (HFunctor sig, HFunctor sig') =>
-        (forall m. sig m ~> sig' m) ->
+        (sig (h sig') ~> sig' (h sig')) ->
         h sig a ->
         h sig' a
     translateH phi = interpretH $ liftSig . phi
@@ -28,8 +28,63 @@ class (forall sig. HFunctor sig => c (h sig)) => Heftia c h | h -> c where
     reinterpretH = interpretH
     {-# INLINE reinterpretH #-}
 
-send :: (s <: t, Heftia c h, HFunctor s, HFunctor t) => s (h s) a -> h t a
-send = liftSig . weakenSig . hmap (translateH weakenSig)
+send :: (Heftia c h, Union u, Member u e es, HFunctor (u es)) => e (h (u es)) ~> h (u es)
+send = liftSig . inject
+
+interpret ::
+    (Heftia c h, Union u, HFunctor (u es), HFunctor (u (e : es))) =>
+    (e (h (u es)) ~> h (u es)) ->
+    h (u (e ': es)) ~> h (u es)
+interpret i =
+    interpretH \u ->
+        case decomp u of
+            Left e -> i e
+            Right e -> liftSig e
+
+reinterpret ::
+    (Heftia c h, Union u, HFunctor (u (e : es))) =>
+    (e (h (u (e ': es))) ~> h (u (e ': es))) ->
+    h (u (e ': es)) ~> h (u (e ': es))
+reinterpret i =
+    reinterpretH \u ->
+        case decomp u of
+            Left e -> i e
+            Right e -> liftSig $ weakenR e
+
+translate ::
+    (Heftia c h, Union u, HFunctor (u (e : es)), HFunctor (u (e' : es))) =>
+    (e (h (u (e' ': es))) ~> e' (h (u (e' ': es)))) ->
+    h (u (e ': es)) ~> h (u (e' ': es))
+translate f =
+    translateH \u ->
+        case decomp u of
+            Left e -> weakenL $ f e
+            Right e -> weakenR e
+
+interpose ::
+    forall e h u es c.
+    (Heftia c h, Union u, Member u e es, HFunctor (u es)) =>
+    (e (h (u es)) ~> h (u es)) ->
+    h (u es) ~> h (u es)
+interpose f = reinterpretH \u ->
+    let u' = hmap (interpose f) u
+     in case project @_ @e u' of
+            Just e -> f e
+            Nothing -> liftSig u'
+
+intercept ::
+    forall e h u es c.
+    (Heftia c h, Union u, Member u e es, HFunctor (u es)) =>
+    (e (h (u es)) ~> e (h (u es))) ->
+    h (u es) ~> h (u es)
+intercept f = translateH \u ->
+    let u' = hmap (intercept f) u
+     in case project @_ @e u' of
+            Just e -> inject $ f e
+            Nothing -> u'
 
 retract :: (Heftia c h, c m) => h (LiftIns m) a -> m a
 retract = interpretH unliftIns
+
+sendH :: (s <: t, Heftia c h, HFunctor s, HFunctor t) => s (h s) a -> h t a
+sendH = liftSig . weakenSig . hmap (translateH weakenSig)
