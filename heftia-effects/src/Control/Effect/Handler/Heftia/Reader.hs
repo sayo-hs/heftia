d038cb8e81f9225e2dd3ffb4ad5445d1f2b58a73
Yamada Ryo <ymdfield@outlook.jp>
2023-09-02 21:45:02 +0900

    [fix] Simplify code with products from the new TH mechanism.

--- heftia-effects/src/Control/Effect/Handler/Heftia/Reader.hs
+++ heftia-effects/src/Control/Effect/Handler/Heftia/Reader.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -6,19 +8,19 @@ module Control.Effect.Handler.Heftia.Reader where
 
 import Control.Effect.Class (LiftIns (LiftIns), type (~>))
 import Control.Effect.Class.HFunctor (HFunctor)
-import Control.Effect.Class.Reader (AskF (Ask), LocalH (Local), ask)
+import Control.Effect.Class.Reader (AskI (AskI), AskS, LocalS (Local), ask, pattern Ask)
 import Control.Heftia (interpret, reinterpret)
 import Control.Heftia.Final (Hef)
 import Data.Hefty.Sum (Sum)
 
-interpretReader :: HFunctor (Sum es) => r -> Hef (LocalH r ': LiftIns (AskF r) ': es) ~> Hef es
+interpretReader :: HFunctor (Sum es) => r -> Hef (LocalS r ': AskS r ': es) ~> Hef es
 interpretReader r = interpretAsk r . elaborateReader
 
 elaborateReader ::
     HFunctor (Sum es) =>
-    Hef (LocalH r ': LiftIns (AskF r) ': es) ~> Hef (LiftIns (AskF r) ': es)
+    Hef (LocalS r ': AskS r ': es) ~> Hef (AskS r ': es)
 elaborateReader = interpret \(Local f a) ->
-    ($ a) $ reinterpret \(LiftIns Ask) -> f <$> ask
+    ($ a) $ reinterpret \Ask -> f <$> ask
 
-interpretAsk :: HFunctor (Sum es) => r -> Hef (LiftIns (AskF r) ': es) ~> Hef es
-interpretAsk r = interpret \(LiftIns Ask) -> pure r
+interpretAsk :: HFunctor (Sum es) => r -> Hef (AskS r ': es) ~> Hef es
+interpretAsk r = interpret \(LiftIns AskI) -> pure r
