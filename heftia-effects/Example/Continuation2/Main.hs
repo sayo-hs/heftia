8f71a2d4e6125018b64cbbacd32151565a29046d
Yamada Ryo <ymdfield@outlook.jp>
2024-07-15 20:14:00 +0900

    [refactor] the 'Continuation2' example.

--- heftia-effects/Example/Continuation2/Main.hs
+++ heftia-effects/Example/Continuation2/Main.hs
@@ -4,100 +4,102 @@
 
 module Main where
 
+import Control.Arrow ((>>>))
 import Control.Effect.ExtensibleChurch (runEff, type (!!))
 import Control.Effect.Handler.Heftia.Reader (elaborateLocal, interpretAsk)
 import Control.Effect.Handler.Heftia.ShiftReset (runShift, runShift_)
 import Control.Effect.Handler.Heftia.State (evalState)
-import Control.Effect.Hefty (interpretH, send1, type ($))
+import Control.Effect.Hefty (interpretH, send1, unkeyEff, type ($))
+import Control.Effect.Key (key)
 import Control.Monad.Extra (whenM)
 import Control.Monad.IO.Class (liftIO)
 import Data.Effect.HFunctor ((:+:))
+import Data.Effect.Key (type (#>))
 import Data.Effect.Reader (Ask, Local, ask, local)
 import Data.Effect.ShiftReset (Shift, Shift_, getCC, getCC_)
-import Data.Effect.State (State, get, modify)
+import Data.Effect.State (State, get, get'', modify)
 import Data.Free.Sum (type (+))
 import Data.Function ((&))
 import Data.Functor ((<&>))
 
 main :: IO ()
 main = do
-    putStrLn "[elaborateLocalThenShift]"
-    elaborateLocalThenShift
+    putStrLn "[handleReaderThenShift]"
+    handleReaderThenShift
 
     putStrLn ""
-    putStrLn "[elaborateShiftThenLocal]"
-    elaborateShiftThenLocal
+    putStrLn "[handleShiftThenReader]"
+    handleShiftThenReader
 
 {-
 ===== result =====
 
-[elaborateLocalThenShift]
-[local scope outer] env = 1.0
-[local scope inner] env = 2.0
-[local scope outer] env = 1.0
-[local scope inner] env = 2.0
-[local scope outer] env = 1.0
-[local scope inner] env = 2.0
-[local scope outer] env = 1.0
-[local scope inner] env = 2.0
-[local scope outer] env = 1.0
-[local scope inner] env = 2.0
-[local scope outer] env = 1.0
+[handleReaderThenShift]
+[local scope outer] env = 1
+[local scope inner] env = 2
+[local scope outer] env = 1
+[local scope inner] env = 2
+[local scope outer] env = 1
+[local scope inner] env = 2
+[local scope outer] env = 1
+[local scope inner] env = 2
+[local scope outer] env = 1
+[local scope inner] env = 2
+[local scope outer] env = 1
 
-[elaborateShiftThenLocal]
-[local scope outer] env = 1.0
-[local scope inner] env = 2.0
-[local scope outer] env = 2.0
-[local scope inner] env = 4.0
-[local scope outer] env = 4.0
-[local scope inner] env = 8.0
-[local scope outer] env = 8.0
-[local scope inner] env = 16.0
-[local scope outer] env = 16.0
-[local scope inner] env = 32.0
-[local scope outer] env = 32.0
-
+[handleShiftThenReader]
+[local scope outer] env = 1
+[local scope inner] env = 2
+[local scope outer] env = 2
+[local scope inner] env = 4
+[local scope outer] env = 4
+[local scope inner] env = 8
+[local scope outer] env = 8
+[local scope inner] env = 16
+[local scope outer] env = 16
+[local scope inner] env = 32
+[local scope outer] env = 32
 -}
 
-elaborateLocalThenShift :: IO ()
-elaborateLocalThenShift =
+handleReaderThenShift :: IO ()
+handleReaderThenShift =
     prog
         & interpretH elaborateLocal
-        & interpretAsk 1.0
+        & interpretAsk 1
         & runEff
         & runShift
-        & evalState 0
+        & (unkeyEff >>> evalState 0)
         & runEff
   where
-    prog :: Local Double !! Ask Double + Shift () !! State Int + IO $ ()
+    prog :: Local Int !! Ask Int + Shift () !! "counter" #> State Int + IO $ ()
     prog = do
         k <- send1 getCC
-        env <- ask @Double
+        env <- ask @Int
         send1 $ liftIO $ putStrLn $ "[local scope outer] env = " ++ show env
-        local @Double (* 2) do
-            whenM (send1 (get @Int) <&> (< 5)) do
-                send1 $ modify @Int (+ 1)
-                env' <- ask @Double
+        local @Int (* 2) do
+            whenM (send1 (get'' @"counter") <&> (< 5)) do
+                send1 $ modify (+ 1) & key @"counter"
+                env' <- ask @Int
                 send1 $ liftIO $ putStrLn $ "[local scope inner] env = " ++ show env'
                 send1 k
 
-elaborateShiftThenLocal :: IO ()
-elaborateShiftThenLocal = do
+handleShiftThenReader :: IO ()
+handleShiftThenReader = do
     prog
         & runShift_
         & interpretH elaborateLocal
-        & interpretAsk 1.0
-        & evalState 0
+        & interpretAsk 1
+        & (unkeyEff >>> evalState 0)
         & runEff
   where
-    prog :: Shift_ :+: Local Double !! Ask Double + State Int + IO $ ()
+    prog :: Shift_ :+: Local Int !! Ask Int + "counter" #> State Int + IO $ ()
     prog = do
         k <- getCC_
-        env <- ask @Double
+        env <- ask @Int
         liftIO $ putStrLn $ "[local scope outer] env = " ++ show env
-        local @Double (* 2) do
-            whenM (get @Int <&> (< 5)) do
-                modify @Int (+ 1)
-                env' <- ask @Double
+        local @Int (* 2) do
+            whenM (get'' @"counter" <&> (< 5)) do
+                modify (+ 1) & key @"counter"
+                env' <- ask @Int
                 liftIO $ putStrLn $ "[local scope inner] env = " ++ show env'
                 k
