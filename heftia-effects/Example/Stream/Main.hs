55e5da5cf782033550780243ed6d566021938564
Yamada Ryo <ymdfield@outlook.jp>
2024-10-27 12:30:16 +0900

    [remove] dead code in the stream example.

--- heftia-effects/Example/Stream/Main.hs
+++ heftia-effects/Example/Stream/Main.hs
@@ -7,42 +7,17 @@ module Main where
 
 import Control.Arrow ((>>>))
 import Control.Monad (forever, void, when)
-import Control.Monad.Hefty (
-    Eff,
-    interpret,
-    liftIO,
-    raiseAllH,
-    (&),
-    type (<:),
-    type (<<|),
-    type (<|),
-    type (~>),
- )
+import Control.Monad.Hefty (Eff, liftIO, raiseAllH, type (<:), type (<|))
 import Control.Monad.Hefty.Concurrent.Parallel (runParallelIO)
 import Control.Monad.Hefty.Concurrent.Stream (Machinery (Unit), runMachinery, runMachineryIO)
 import Control.Monad.Hefty.Concurrent.Timer (Timer, runTimerIO, sleep)
 import Control.Monad.Hefty.Except (runThrow, throw)
 import Control.Monad.Hefty.Input (Input, input)
 import Control.Monad.Hefty.Output (Output, output)
-import Control.Monad.Hefty.Resource (Resource, bracket_, runResourceIO)
+import Control.Monad.Hefty.Resource (bracket_, runResourceIO)
 import Control.Monad.Hefty.Unlift (runUnliftIO)
 import Control.Monad.IO.Class (MonadIO)
 import Data.Foldable (for_)
-
-data SomeIOResource a where
-    ReadResource :: SomeIOResource Int
-    WriteResource :: Int -> SomeIOResource ()
-
-runSomeResource
-    :: (Resource <<| eh, IO <| ef)
-    => Eff eh (SomeIOResource ': ef) ~> Eff eh ef
-runSomeResource m = bracket_
-    (liftIO $ putStrLn "Acquiring resource")
-    (liftIO $ putStrLn "Releasing resource")
-    do
-        m & interpret \case
-            ReadResource -> error "just dummy"
-            WriteResource _ -> error "just dummy"
 
 {- | In reality, this 'throw' operates independently of @bracket@...
 because 'runThrow' functions under the semantics of pure algebraic effects,
